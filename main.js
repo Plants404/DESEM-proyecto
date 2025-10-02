239ed5cbab6f517a2c2edb3d4fd3f02ceba6b174
Plants404 <marcio.lahoura21tate@gmail.com>
2025-10-02 20:25:05 -0300

    fist commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "none") {
+     div.style.display = "flex";
+    }
+    else {
+        div.style.display = "none";
+    }   
+}
+
+const myCarouselElement = document.querySelector('#carouselExampleIndicators')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 300,
+  touch: true
+})
